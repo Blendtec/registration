3803623cc3f2c0988efb3eb93e1fb95330c4c365
Reid Kuttler <rkuttler@blendtec.com>
2017-12-15 14:21:20 -0700

    first draft for registration success section

--- src/app/request.service.ts
+++ src/app/request.service.ts
@@ -7,23 +7,25 @@ export class RequestService {
 
   constructor(private http: HttpClient) { }
 
-  private handleError(error: any): Promise<any> {
+  private handleError(error: any, errorCallback = null): Promise<any> {
     console.error('An error occurred', error);
+    errorCallback(error);
     return Promise.reject(error.message || error);
   }
 
-  submitData(submitUrl: string, data: any, callback = null) {
+  submitData(submitUrl: string, data: any, callback = null, errorCallback = null) {
     return this.http.post(submitUrl, data, {responseType: 'text'})
       .toPromise()
       .then(response => {
         response = response;
-        console.log(response);
-        if (callback.constructor === Function) {
+        if (callback.constructor === Function && response === 'success') {
           callback();
+        } else if (errorCallback.constructor === Function && response != 'success') {
+          errorCallback(response);
         }
 
       })
-      .catch(e => this.handleError(e));
+      .catch(e => this.handleError(e, errorCallback));
   }
 
   httpGet(submitUrl: string, callback = null) {
