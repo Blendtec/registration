424457a0a6ca1879e1aef0a7ce8becfd901253de
Stone C. Lasley <stonelasley@gmail.com>
2017-12-28 13:03:20 -0700

    fix(dependencies): Add ng-http-loader to package.json
    BTCOM-2231

--- src/app/services/registration.service.spec.ts
+++ src/app/services/registration.service.spec.ts
@@ -2,6 +2,7 @@ import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { RegistrationService } from './registration.service';
+import { NgHttpLoaderModule } from 'ng-http-loader/ng-http-loader.module';
 
 
 describe('RegistrationService', () => {
