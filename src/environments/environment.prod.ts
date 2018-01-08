24845a9669e0b6851ef38106a635971ba3bc4428
Stone C. Lasley <stonelasley@gmail.com>
2018-01-08 11:34:09 -0700

    fix(I18n): Fix Production Asset url
    BTCOM-2239

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,6 +4,6 @@ export const environment = {
   registrationEndpoint: 'product_registrations/addApi',
   bucket: 'https://s3-us-west-1.amazonaws.com/recipe-client.blendtec.com/registration',
   s3: 'https://s3-us-west-1.amazonaws.com/data.blendtec.com',
-  assets: 'https://s3-us-west-1.amazonaws.com/data.blendtec.com/registration/assets',
+  assets: 'https://s3-us-west-1.amazonaws.com/recipe-client.blendtec.com/assets',
   captchaKey: '6LcWmzIUAAAAADoSNPMqAECfcdIl9Z8B4czc4MjP'
 };
