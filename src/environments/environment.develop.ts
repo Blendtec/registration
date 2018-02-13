bc022941163ea39f2fc53f777699a86f6068f655
Stone C. Lasley <stonelasley@gmail.com>
2018-02-13 15:05:55 -0700

    ci(travis): Simplify Travis Config

---
+++ src/environments/environment.develop.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+  apiHost: 'https://www.blendtec.com',
+  registrationEndpoint: 'product_registrations/addApi',
+  bucket: 'https://s3-us-west-1.amazonaws.com/apps.develop.blendtec.com/registration',
+  s3: 'https://s3-us-west-1.amazonaws.com/data.blendtec.com',
+  assets: 'https://s3-us-west-1.amazonaws.com/apps.develop.blendtec.com/assets',
+  captchaKey: '6LcWmzIUAAAAADoSNPMqAECfcdIl9Z8B4czc4MjP'
+};
