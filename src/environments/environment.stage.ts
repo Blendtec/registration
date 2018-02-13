78e2377f7a76f4d035b18a772bb76dfa219d0a45
Stone C. Lasley <stonelasley@gmail.com>
2018-02-13 15:41:18 -0700

    fix(environments): bucket paths

--- src/environments/environment.stage.ts
+++ src/environments/environment.stage.ts
@@ -2,8 +2,8 @@ export const environment = {
   production: true,
   apiHost: 'https://www.blendtec.com',
   registrationEndpoint: 'product_registrations/addApi',
-  bucket: 'https://s3-us-west-1.amazonaws.com/apps.stage.blendtec.com/registration',
+  bucket: 'https://s3-us-west-1.amazonaws.com/apps.stage.blendtec.com/product-registration',
   s3: 'https://s3-us-west-1.amazonaws.com/data.blendtec.com',
-  assets: 'https://s3-us-west-1.amazonaws.com/apps.stage.blendtec.com/registration/assets',
+  assets: 'https://s3-us-west-1.amazonaws.com/apps.stage.blendtec.com/product-registration/assets',
   captchaKey: '6LcWmzIUAAAAADoSNPMqAECfcdIl9Z8B4czc4MjP'
 };
