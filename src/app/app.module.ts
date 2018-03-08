10e69ff09559b174b0a9740c71cd712ce9eeebdb
Stone C. Lasley <stonelasley@gmail.com>
2018-03-08 14:05:07 -0700

    fix(ProductRegistration): Remove Loader, Remove Header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { Inject, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { RegistrationComponent } from './registration/registration.component';
 import { SuccessComponent } from './success/success.component';
-import { StoreService } from './services/store.service';
+import {
+  CountryService, RegistrationService, RetailerService, SerialPrefixService, StateService, StoreService,
+  WindowService
+} from './services';
 import { AppRoutingModule } from './app-routing.module';
 import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { RecaptchaModule } from 'ng-recaptcha';
-import { APP_BASE_HREF } from '@angular/common';
-import { RegistrationService } from './services/registration.service';
-import { WindowService } from './services/window.service';
-import { CountryService } from './services/country.service';
-import { RetailerService } from './services/retailer.service';
 import { MyDatePickerModule } from 'mydatepicker';
-import { StateService } from './services/state.service';
 import { NgPipesModule } from 'ngx-pipes';
 import { AppConfigModule } from './config';
-import { NgHttpLoaderModule } from 'ng-http-loader/ng-http-loader.module';
 import { TranslateLoader, TranslateModule, TranslatePipe } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { CountrySelectComponent } from './directives/country-select/country-select.component';
-import { AppConfig } from './config/models/app-config.interface';
-import { APP_CONFIG } from './config/app-config.module';
+import { AppConfig, APP_CONFIG } from './config';
 import { SerialModalComponent } from './directives/serial-modal.component/serial-modal.component';
-import { SerialPrefixService } from './services/serial-prefix.service';
 
 export function HttpLoaderFactory(http: HttpClient, config: AppConfig) {
   return new TranslateHttpLoader(http, `${config.assets}/i18n/`, '.json');
@@ -48,7 +42,6 @@ export function HttpLoaderFactory(http: HttpClient, config: AppConfig) {
     FormsModule,
     HttpClientModule,
     MyDatePickerModule,
-    NgHttpLoaderModule,
     NgPipesModule,
     ReactiveFormsModule,
     RecaptchaModule.forRoot(),
