1dee087b18588bf274f2b8d41f55fef888f0fc08
Reid Kuttler <reidkuttler@gmail.com>
2018-09-12 12:12:35 -0700

    fix(travis): got rid of unnecessary library in shopify deploy

--- .travis/shopify.js
+++ .travis/shopify.js
@@ -1,7 +1,6 @@
 var fs = require("fs");
 var path = require("path");
 var ShopifyAPI = require("shopify-node-api");
-var striptags = require('striptags');
 var args = process.argv.slice(2);
 
 var Shopify = new ShopifyAPI({
@@ -15,7 +14,7 @@ fs.readFile(indexPagePath, "utf-8", function (err, data) {
   if (!err) {
     var put_data = {
       page: {
-        body_html: striptags(data, ['app-root', 'base', 'script'])
+        body_html: data
       }
     };
     Shopify.put("/admin/pages/" + args[3]+ ".json", put_data, function (err, data, headers) {
